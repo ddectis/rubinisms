de7826a6feede11ef5241c7f6a016741047e7c4a
Dan Dectis <dandectis@gmail.com>
2024-04-11 16:24:12 -0700

    Revert "swipe progress. Still buggy, not able to proceed through the stack as expected"
    This reverts commit fdf20c8e638381cae0238fe96e2769419a818f05.

--- src/components/Swipe.jsx
+++ src/components/Swipe.jsx
@@ -74,7 +74,6 @@ export default function Swipe({ content, actionOnDismiss, cardIndex }) {
          //       initialTouchPosition
          // );
          measureSwipeDistance();
-         console.log("CardID: " + cardIndex)
       }
    };
 
@@ -129,9 +128,9 @@ export default function Swipe({ content, actionOnDismiss, cardIndex }) {
             slider?.classList.add(styles.dismissLeft);
          }
          actionOnDismiss();
-         //setPosition(0);
-         //setScreenPosition(0);
-         //setSwipeDistance(0);
+         setPosition(0);
+         setScreenPosition(0);
+         setSwipeDistance(0);
       } else {
          setPosition(0);
          setScreenPosition(0);
@@ -184,3 +183,41 @@ export default function Swipe({ content, actionOnDismiss, cardIndex }) {
       </>
    );
 }
+
+const holder = () => {
+   return (
+      <>
+         <div
+            className={styles.content}
+            onMouseDown={handleMouseDown}
+            onMouseUp={handleMouseUp}
+            onMouseMove={handleMouseMove}
+            onTouchStart={handleTouchStart}
+            onTouchMove={handleTouchMove}
+            onTouchEnd={handleTouchStop}
+         >
+            <div
+               className={`${styles.opacityTransition} ${styles.fadeIn}`}
+               style={{
+                  position: "absolute",
+                  transform: `translateX(${screenPosition}%)`,
+                  top: 50,
+               }}
+               id="slider"
+            >
+               <div className={homeStyles.quote}>
+                  <div className={homeStyles.quoteText}>
+                     <div className={homeStyles.quoteHeading}>
+                        <h2>Rubinisms</h2>
+                        <h3 className={homeStyles.quoteIndex}>
+                           {quoteIndex} of {quotes.length}
+                        </h3>
+                     </div>
+                     {selectedQuote && <p>{selectedQuote.text}</p>}
+                  </div>
+               </div>
+            </div>
+         </div>
+      </>
+   );
+};
